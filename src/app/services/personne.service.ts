1b9c8263cb0be095a871d7ab805f0a19d2d5fabe
JeremyVk <jeremvk@outlook.fr>
2022-06-21 14:24:58 +0200

    liaison avec back, bootstrap et fontawesome

---
+++ src/app/services/personne.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Personne} from '../interfaces/personne'
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonneService {
+  private url: string = 'http://127.0.0.1:8000/ws/personnes';
+  constructor(private http: HttpClient) { }
+
+  getAllPersons() {
+    return this.http.get<Array<Personne>>(this.url);
+  }
+}
